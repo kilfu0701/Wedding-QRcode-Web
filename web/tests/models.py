ab7999144b4249d4b0b99b9447741fe3298ef6e7
kilfu0701 <kilfu0701@gmail.com>
2014-09-05 12:36:33 +0900

    update

--- web/tests/models.py
+++ web/tests/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class CI_Session(models.Model):
+    session_id = models.AutoField(primary_key=True)
+    ip_address = models.CharField(max_length=45, null=False, blank=False)
+    user_agent = models.CharField(max_length=120, null=False, blank=False)
+    last_activity = models.IntegerField(default=0)
+    prev_activity = models.IntegerField(default=0)
+    user_data = models.TextField()
+   
+    class Meta:
+        db_table = 'ci_sessions'
+

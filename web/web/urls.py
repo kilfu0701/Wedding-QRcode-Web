9d77c8aa5150cca3b36da5263e6652440b98afbd
kilfu0701 <kilfu0701@gmail.com>
2014-09-10 10:17:50 +0800

    remove unsesd files.

--- web/web/urls.py
+++ web/web/urls.py
@@ -6,8 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    # Examples:
-    # url(r'^$', 'web.views.home', name='home'),
+    url(r'^$', 'wedding.views.index'),
     # url(r'^web/', include('web.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
